910a17a77f38c2e99ffe95cb098f14fb711989ee
RodrigoUrrutiaC <luisrurrutiac@gmail.com>
2018-03-11 18:17:21 -0600

    Robot funciona con dos sensores. Hay que conseguir otro

--- robotlaberinto.c
+++ robotlaberinto.c
@@ -1,17 +1,119 @@
-/*
-  Blank Simple Project.c
-  http://learn.parallax.com/propeller-c-tutorials 
-*/
-#include "simpletools.h"                      // Include simple tools
 
-int main()                                    // Main function
+#include "simpletools.h"                      
+#include "abdrive.h"                        
+#include "ping.h"                          
+
+// Declaración de variables
+
+int sensorFrontal, sensorDerecho; //dos sensores                               
+int laberinto[50][50]; //matriz con la posicion del robot
+int spd, ejeX, ejeY, orientacion; //Variables de funcionamiento del programa
+int i,j; //Contadores
+
+
+// Programa principal
+
+int main()                              
 {
-  // Add startup code here.
+  pause(1000);
 
- 
+  ejeX = 0;
+  ejeY = 0;
+  orientacion = 0;
+  
   while(1)
-  {
-    // Add main loop code here.
+  {                       
+    sensorFrontal = ping_cm(8);
+    sensorDerecho = ping_cm(10);
+    i = 0;
     
+    
+    while(sensorFrontal >= 10 && sensorDerecho < 25) // el robot camina hacia enfrente 
+    {
+      sensorFrontal = ping_cm(8);
+      sensorDerecho = ping_cm(10); 
+      
+      drive_ramp(50,50);
+     
+      if(i == 50){       
+        coordenadas();
+        contador();
+        i = 0;
+      }else{
+        i++;
+      }   
+      
+      pause(5);            
+    }
+   
+  
+    if(sensorDerecho < 25) //el robot gira a la izquierda
+    {
+      drive_speed(-30,30);
+      j = 0;
+      while(j < 97)
+      {
+        j++;  
+        pause(5);
+      }        
+      
+      orientacion = orientacion + 3;
+      orientacion = orientacion % 4;             
+        
+    }else{  // el robot gira a la derecha
+            // los siguientes tres while solo hacen que el giro no sea tan abrupto
+      j = 0;
+      while(j < 160)
+      {
+        drive_ramp(30,30);
+        j++;  
+        pause(5);
+      }
+      
+      
+      drive_speed(30,-30);
+      j = 0;
+      while(j < 148)
+      {
+        j++;  
+        pause(5);
+      }
+      
+      
+      drive_speed(50,50);  
+      j = 0;
+      while(j < 180)
+      {
+        j++;  
+        pause(5);
+      }    
+      
+      orientacion++;
+      orientacion = orientacion % 4;
+    }      
+ 
   }  
+                         
 }
+
+// METODOS
+
+void contador(){
+  laberinto[ejeX][ejeY]= laberinto[ejeX][ejeY]+1;
+}  
+
+int coordenadas(){
+  if(orientacion == 0){
+    ejeY++;
+  }    
+  if(orientacion == 1){
+    ejeX++;
+  }
+  if(orientacion == 2){
+    ejeY--;
+  }
+  if(orientacion == 3){
+    ejeX--;
+  }  
+} 
+
